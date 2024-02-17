28564ae834e0bef174a1895be9a94c774fb7de97
howong <howardwongjunyen@gmail.com>
2024-02-17 19:35:53 +0000

    Added build setup and requirements

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name="timeseries",
+    version="1.0",
+    packages=[
+        "timeseries",
+        "timeseries.utils",
+        "timeseries.univariate",
+        "timeseries.multivariate",
+    ],
+    url="https://github.com/howardwong97/timeseries",
+    license="",
+    author="howong",
+    author_email="howardwongjunyen@gmail.com",
+    description="",
+)
